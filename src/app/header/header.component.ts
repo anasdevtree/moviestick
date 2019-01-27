292936b548d5e91f45673841d0fd1c6d46f51676
Anas Iqbal <im.anasiqbal@gmail.com>
2019-01-27 23:45:17 +0400

    Header AuthService access limit solved

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,8 +6,12 @@ import { AuthService } from "../auth/auth.service";
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   constructor(private authService: AuthService) {}
 
   ngOnInit() {}
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
